017f7a03f11db5728bc2a9fe472201e34b77f896
Martin <martyn.horshovskyy@gmail.com>
2023-07-31 15:50:50 +0300

    Solve 4

---
+++ src/bin/04.rs
@@ -0,0 +1,56 @@
+use md5::compute;
+
+pub fn part_one(mut input: &str) -> Option<u32> {
+    let mut result: u32 = 0;
+    input = input.trim();
+
+    while {
+        let md5 = compute(format!("{input}{result}"));
+        let md5_string = format!("{:x}", md5);
+
+        &md5_string[0..5] != "00000"
+    } {
+        result += 1
+    }
+
+    Some(result)
+}
+
+pub fn part_two(mut input: &str) -> Option<u32> {
+    let mut result: u32 = 0;
+    input = input.trim();
+
+    while {
+        let md5 = compute(format!("{input}{result}"));
+        let md5_string = format!("{:x}", md5);
+
+        &md5_string[0..6] != "000000"
+    } {
+        result += 1
+    }
+
+    Some(result)
+}
+
+fn main() {
+    let input = &advent_of_code::read_file("inputs", 4);
+    advent_of_code::solve!(1, part_one, input);
+    advent_of_code::solve!(2, part_two, input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = advent_of_code::read_file("examples", 4);
+        assert_eq!(part_one(&input), None);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = advent_of_code::read_file("examples", 4);
+        assert_eq!(part_two(&input), None);
+    }
+}
